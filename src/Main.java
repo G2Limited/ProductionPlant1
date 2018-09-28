c6539ddac8ffdd30f8a0879f04e59f64ccfd0f63
Garrett <ggraber7402@eagle.fgcu.edu>
2018-09-28 12:45:52 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
-    public static void main(String[] args) {
-      Widget w1 = new Widget();
-      System.out.println(w1.toString());
-      Widget w2 = new Widget();
-      System.out.println(w2.toString());
-    }
+  public static void main(String[] args) {
+    Widget w1 = new Widget();
+    System.out.println(w1.toString());
+    Widget w2 = new Widget();
+    System.out.println(w2.toString());
+  }
 }
