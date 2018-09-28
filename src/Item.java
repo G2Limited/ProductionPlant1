cec5211f32ae617b2800dd5173d8462989a7e257
Garrett <ggraber7402@eagle.fgcu.edu>
2018-09-28 12:34:04 -0400

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,11 @@
+import java.util.Date;
+
 public interface Item {
+  String manufacturer = "OraclProduction";
   public void setProductionNumber(int productionNumber);
   public void setName(String name);
-  public void
+
+  public String getName();
+  public Date getManufactureDate();
+  public int getSerialNumber();
 }
