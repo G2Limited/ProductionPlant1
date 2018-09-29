526bed01ae40cd3e0867ee9b2f2b3d6bfecef5dd
Garrett <ggraber7402@eagle.fgcu.edu>
2018-09-29 19:15:39 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
+/*Garrett Graber
+09/29/2018
+Sources used: Professor Scott VanSelow, https://www.javatpoint.com/ , https://docs.oracle.com/javase/tutorial/java
+This file creates the driver class Main */
+
 public class Main {
 
   public static void main(String[] args) {
+    //create new objects from class Widget
     Widget w1 = new Widget();
     System.out.println(w1.toString());
     Widget w2 = new Widget();
