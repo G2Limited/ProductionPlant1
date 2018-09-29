70d7ab160625c1a6dfa31b281629e761ea2fd9f3
Garrett <ggraber7402@eagle.fgcu.edu>
2018-09-29 19:14:27 -0400

    Update ItemType.java

--- src/ItemType.java
+++ src/ItemType.java
@@ -1,10 +1,16 @@
-//Create an enum called ItemType that will
-//store the following information
-//Type          Code
-//Audio          AU
-//Visual         VI
-//AudioMobile    AM
-//VisualMobile   VM
+/*Garrett Graber
+09/29/2018
+This file creates an enum of ItemType and will store information
+for the Item Type and Code*/
+
+/*All items will have a pre-set type. Currently there are 4 types.
+Create an enum called ItemType that will
+store the following information
+Type          Code
+Audio          AU
+Visual         VI
+AudioMobile    AM
+VisualMobile   VM */
 
 public enum ItemType {
   AUDIO("AU"),
@@ -12,14 +18,15 @@ public enum ItemType {
   AUDIO_MOBILE("AM"),
   VISUAL_MOBILE("VM");
 
+
   private final String code;
 
   ItemType(String code) {
     this.code = code;
   }
 
+  //get method for returning the value code
   public String getCode() {
     return code;
   }
 }
-
